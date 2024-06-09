f295d07cc9ebce1de9fbba51d4b4d357de7fc1c6
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-06-09 11:00:09 +0530

    updated

--- src/component/Restaurant.js
+++ src/component/Restaurant.js
@@ -1,14 +1,31 @@
 import { Counter } from "./Counter";
+import { useState } from "react"
 
 export function Restaurant({ restaurantData }) {
   console.log(restaurantData);
+  const [show, setShow] = useState(true)
+
+  const styles = {
+    margin: "0px",
+    color: restaurantData.average_rating >= 4 ? "green" : "red"
+  }
+
+  // true => block
+  // false => none
+
+  const AddressStyle = {
+    display: show ? "block" : "none"
+  }
 
   return (
     <div className="restaurant-card">
       <img className="restaurant-poster" src={restaurantData.restaurant_thumb} alt={restaurantData.restaurant_name} />
       <h3 className="restaurant-name">{restaurantData.restaurant_name}</h3>
-      <h5 style={{ margin: "0px" }}>Address - {restaurantData.address}</h5>
-      <p style={{ margin: "0px" }}>Rating: ⭐{restaurantData.average_rating} {restaurantData.rating_text}</p>
+
+      <button onClick={() => setShow(!show)}>Toggle description</button>
+
+      <p style={AddressStyle}>Address - {restaurantData.address}</p>
+      <p style={styles}>Rating: ⭐{restaurantData.average_rating} {restaurantData.rating_text}</p>
       <div className="style-card">
         <p>Cost: Rs {restaurantData.cost}/-</p>
         <button>Add to Cart</button>
